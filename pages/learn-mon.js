283f8cbeeb68e1382c8a80eaa1e00a7d128553dd
Htaw Mon <htawmon123@gmail.com>
2025-08-15 00:47:14 -0400

    Add new page for learning Mon digits with interactive game

--- pages/learn-mon.js
+++ pages/learn-mon.js
@@ -1,11 +1,17 @@
 import styles from '../styles/LearnMon.module.css';
+import Link from 'next/link';
 
 export default function LearnMon() {
     return (
         <div className={styles.container}>
             <h1>Learn Mon</h1>
-            <p>Updates Under Construction!</p>
+            <p>
+                <Link href="/learn-mon-digits">
+                    Click here to learn Mon digits with our interactive game →
+                </Link>
+            </p>
         </div>
     );
 }
+
 
